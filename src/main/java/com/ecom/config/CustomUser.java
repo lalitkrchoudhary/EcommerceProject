b197432b7fd550093b18e4adc39e2a3085b679b5
lalit8694 <lalit8694123@gmail.com>
2024-11-27 23:04:32 +0530

    working on login page

--- src/main/java/com/ecom/config/CustomUser.java
+++ src/main/java/com/ecom/config/CustomUser.java
@@ -1,8 +1,11 @@
 package com.ecom.config;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.ecom.model.UserDtls;
@@ -11,21 +14,44 @@ public class CustomUser implements UserDetails {
 
 	private UserDtls user;
 
+	
+	
+	public CustomUser(UserDtls user) {
+		super();
+		this.user = user;
+	}
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+		return Arrays.asList(authority);
 	}	
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return user.getPassword();
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return user.getEmail();
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
